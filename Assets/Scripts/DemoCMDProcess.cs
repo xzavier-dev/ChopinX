059f816f96729ab937b915386470fd8e5d90af94
xzavier <xzavier.dev@gmail.com>
2014-05-09 17:58:24 +0800

    First Commit

---
+++ Assets/Scripts/DemoCMDProcess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using GameSocket;
+using UnityEngine;
+
+
+/// <summary>
+/// DemoCMDProcess 的摘要说明
+/// 
+/// <para>____________________________________________________________</para>
+/// <para>Version：V1.0.0</para>
+/// <para>Namespace：GameSocket</para>
+/// <para>Author: wboy    Time：2014/4/25 12:32:18</para>
+/// </summary>
+public class DemoCMDProcess : IProcessCMD
+{
+    private const int packetMaxLength = 10240;
+    private SocketCallBack mCallback;
+
+    private int writeIndex = 0;
+    private byte[] buffer;
+
+    public DemoCMDProcess(SocketCallBack Callback)
+    {
+        this.buffer = new byte[packetMaxLength];
+        this.mCallback = Callback;
+    }
+
+    override public void IncomingData(byte[] data, int actualSize)
+    {
+        if (this.writeIndex + actualSize >= packetMaxLength)
+        {
+            throw new Exception("Buffer Overflow!");
+        }
+        Array.Copy(data, 0, this.buffer, this.writeIndex, actualSize);
+        this.writeIndex += actualSize;
+        Debug.Log("writeIndex ： " + writeIndex + "     " + Demo.packetLength);
+        while (this.writeIndex >= Demo.packetLength)
+        {
+            byte[] bytes = new byte[Demo.packetLength];
+            Array.Copy(this.buffer, 0, bytes, 0, Demo.packetLength);
+            this.mCallback.SendMessage(bytes);
+            this.writeIndex = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using com.CR.GameDataModel;
+using System;
+using System.Text;
+
+public class Test : MonoBehaviour {
+    void Start() {
+        DataCenter.packetProcesser.event_Attack += Callback_Attack;
+
+        Attack attack = new Attack();
+        attack.hurtValue = 1000;
+        attack.weaponId = 100;
+        attack.weaponName = "Weapon 001";
+
+        byte[] data = DataCenter.PacketBuilder.Build( attack );
+
+        DataCenter.PacketParser.Parse( data );
+
+    }
+
+    void OnDiable() {
+        DataCenter.packetProcesser.event_Attack -= Callback_Attack;
+
+    }
+
+    void Callback_Login(Login message) {
+
+    }
+
+    void Callback_Player( Player player ) {
+
+    }
+
+    void Callback_Attack( Attack attack ) {
+        Debug.Log( attack.weaponName + "    " + attack.hurtValue );
+    }
+
+  
+
+  
+
+
+}
